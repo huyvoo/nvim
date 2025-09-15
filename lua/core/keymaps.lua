6cb6ea120bc60534c72c6a1976d833e37e3c3889
Huy Vo <huyvoo345@gmail.com>
2025-09-15 08:49:30 -0700

    Add

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -18,16 +18,3 @@ map("n", "<C-j>", "<C-w>j", opts)
 map("n", "<C-k>", "<C-w>k", opts)
 map("n", "<C-l>", "<C-w>l", opts)
 -- vim.api.nvim_set_keymap("n", "<Leader>e", ":NvimTreeToggle<CR>", opts)
-vim.api.nvim_create_autocmd({ "InsertEnter" }, {
-	pattern = "*",
-	callback = function()
-		vim.opt.relativenumber = false
-	end,
-})
-
-vim.api.nvim_create_autocmd({ "InsertLeave" }, {
-	pattern = "*",
-	callback = function()
-		vim.opt.relativenumber = true
-	end,
-})
